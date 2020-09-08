83f3cec5eadec292901152adeeef4c7916c6f28a
Tom <thomas.spencer@codurance.com>
2020-09-08 14:10:00 +0100

    Passing test for size 3 list

--- src/test/java/com/codurance/SortTest.java
+++ src/test/java/com/codurance/SortTest.java
@@ -18,7 +18,7 @@ public class SortTest {
     assertSorted(unsortedList, sorted);
     assertSorted(sort(intList(1)), intList(1));
     assertSorted(sort(intList(2, 1)), intList(1, 2));
-
+    assertSorted(sort(intList( 2, 1, 3)), intList(1, 2, 3));
   }
 
   private void sortBigList(int n) {
@@ -43,13 +43,26 @@ public class SortTest {
     List<Integer> sorted = new ArrayList<>();
     if (list.size() <= 1)
       return list;
-    if (list.get(0) > list.get(1)){
-      sorted.add(list.get(1));
-      sorted.add(list.get(0));
-    }else {
-      sorted.add(list.get(0));
-      sorted.add(list.get(1));
+    else if (list.size() == 2){
+      if (list.get(0) > list.get(1)){
+        sorted.add(list.get(1));
+        sorted.add(list.get(0));
+      }else {
+        sorted.add(list.get(0));
+        sorted.add(list.get(1));
+      }
+    } else if (list.size() == 3) {
+      if (list.get(1) > list.get(2)){
+        sorted.add(list.get(2));
+        sorted.add(list.get(0));
+        sorted.add(list.get(1));
+      }else {
+        sorted.add(list.get(1));
+        sorted.add(list.get(0));
+        sorted.add(list.get(2));
+      }
     }
+
     return sorted;
   }
 
