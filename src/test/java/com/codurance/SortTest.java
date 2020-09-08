e9ec6fdf05fa89da523a118f92f76aaceb731833
Tom <thomas.spencer@codurance.com>
2020-09-08 11:01:23 +0100

    Initial commit

---
+++ src/test/java/com/codurance/SortTest.java
@@ -0,0 +1,24 @@
+package com.codurance;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class SortTest {
+
+  @Test
+  void sortings() {
+    assertThat(sort(intList()), is(intList()));
+  }
+
+  private List<Integer> intList() {
+    return asList();
+  }
+
+  private List<Integer> sort(List<Integer> list){
+    return null;
+  }
+}
