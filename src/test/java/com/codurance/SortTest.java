0cc6977ee7890f838ea627bf3dc96e683f05e0f7
Tom <thomas.spencer@codurance.com>
2020-09-08 11:10:39 +0100

    Add more split flow statements - duplicate code

--- src/test/java/com/codurance/SortTest.java
+++ src/test/java/com/codurance/SortTest.java
@@ -17,6 +17,7 @@ public class SortTest {
     assertSorted(unsortedList, sorted);
     assertSorted(sort(intList(1)), intList(1));
     assertSorted(sort(intList(2, 1)), intList(1, 2));
+    assertSorted(sort(intList(1, 3, 2)), intList(1, 2, 3));
   }
 
   private void assertSorted(List<Integer> unsortedList, List<Integer> sorted) {
@@ -29,11 +30,17 @@ public class SortTest {
 
   private List<Integer> sort(List<Integer> list){
     if(list.size() > 1) {
-
       if (list.get(0) > list.get(1)) {
         Integer temp = list.get(0);
         list.set(0, list.get(1));
         list.set(1, temp);
+      }
+      if (list.size() > 2){
+        if (list.get(1) > list.get(2)) {
+          Integer temp = list.get(1);
+          list.set(1, list.get(2));
+          list.set(2, temp);
+        }
       }
     }
     return list;
